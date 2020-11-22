1a24c0cfe2f9cc722c16a911ceee19ae53ba4047
LinghaoChan <Linghao_Chan@163.com>
2020-11-22 11:50:03 +0800

    Create data-processing.py

---
+++ Upper/data-processing.py
@@ -0,0 +1,20 @@
+import os
+import numpy
+from matplotlib import pyplot as plt
+import pandas
+import torch
+
+
+class Data_Preprocessing(object):
+    """
+    docstring
+    """
+    def __init__(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
