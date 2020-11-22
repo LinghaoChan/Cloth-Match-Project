70a3165be9e39feda889fd851a595a32090432c9
LinghaoChan <Linghao_Chan@163.com>
2020-11-22 13:25:52 +0800

    casual

--- Upper/data-processing.py
+++ Upper/data-processing.py
@@ -1,6 +1,7 @@
 import os
 import numpy
 from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
 import pandas
 import torch
 
@@ -14,16 +15,27 @@ class Data_Preprocessing(object):
     
     def count_files(self, path: str):
         """
-        docstring
+        Count the number of images in this floder
         """
         print(path, " ", len(os.listdir(path)))
 
-
+    def show_img(self, path = "./Old_fashioned/Old_fashioned_1.jpg"):
+        """
+        Show the image of a image, for debuging.
+        """
+        img = mpimg.imread(path) 
+        print(img.shape) 
+        plt.imshow(img)
+        plt.axis('off')
+        plt.show()
+                
     def read_pic(self):
+        """
+        Get the message of images.
+        """
         path = os.getcwd()
         l = os.listdir(path)
         l.remove("data-processing.py")
-        # print(l)
         for floder in l:
             floder = "./" + floder
             self.count_files(floder)
@@ -31,6 +43,7 @@ class Data_Preprocessing(object):
 def main():
     data_preprocessing = Data_Preprocessing()
     data_preprocessing.read_pic()
+    data_preprocessing.show_img()
 
 if __name__ == "__main__":
     # execute only if run as a script
