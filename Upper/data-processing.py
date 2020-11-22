9389e252ed3e9bdbfe2442a69d641bf31104e17b
LinghaoChan <Linghao_Chan@163.com>
2020-11-22 15:55:01 +0800

    reshape

--- Upper/data-processing.py
+++ Upper/data-processing.py
@@ -3,6 +3,7 @@ import numpy
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 import pandas
+from PIL import Image
 import torch
 
 
@@ -25,18 +26,18 @@ class Data_Preprocessing(object):
         Show the image of a image, for debuging.
         """
         img = mpimg.imread(path)
-        return(img.shape)
         # plt.imshow(img)
         # plt.axis('off')
         # plt.show()
+        # print(img.shape)
+        return(img.shape)
 
     def show_image_message(self, path):
         """
         Show the image infomation in a folder
         """
         filelist = os.listdir(path)
-        total_num = len(filelist)  # 获取文件长度（个数）
-        i = 1  # 表示文件的命名是从1开始的
+        total_num = len(filelist)  # get the number of files
         dic = {}
         for item in filelist:
             if item.endswith('.jpg'):
@@ -55,19 +56,46 @@ class Data_Preprocessing(object):
         path = os.getcwd()
         l = os.listdir(path)
         l.remove("data-processing.py")
+        self.filelist = l
         for floder in l:
             floder = "./" + floder
             self.count_files(floder)
-        image_shape_calculator = []
+        # image_shape_calculator = []
+        # for floder in l:
+        #     self.show_image_message(floder)
+        #     image_shape_calculator.append(self.show_image_message(floder))
+        # self.image_shape_set = image_shape_calculator
+
+    def change_image_size(self, path, size=(150, 100, 3)):
+        """
+        Change the size of an image to size = 30 x 20 x 3
+        """  
+        self.show_img(path) 
+        img = Image.open(path)
+        out = img.resize((size[0], size[1]),Image.ANTIALIAS) #resize image with high-quality
+        out.save(path)   
+        self.show_img(path)
+
+    def batch_chage_image_size(self):
+        """
+        docstring
+        """
+        print(self.filelist)
+        l = self.filelist
         for floder in l:
-            self.show_image_message(floder)
-            print(self.show_image_message(floder))
+            cur_path = "./" + floder
+            img_list = os.listdir(cur_path)
+            for img in img_list:
+                img_path = cur_path + "/" + img
+                print((img_path))
+                self.change_image_size(img_path)
 
 
 def main():
     data_preprocessing = Data_Preprocessing()
     data_preprocessing.read_floder()
-
+    # data_preprocessing.change_image_size("Casual_1.jpg")
+    data_preprocessing.batch_chage_image_size()
 
 if __name__ == "__main__":
     # execute only if run as a script
